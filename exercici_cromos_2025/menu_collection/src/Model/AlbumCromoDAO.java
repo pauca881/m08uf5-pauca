7a3fac0848636dfaea27f54e168f99598358630f
pauca881 <lopezpaud@gmail.com>
2025-02-28 10:36:20 +0100

    ADD Dao created

--- exercici_cromos_2025/menu_collection/src/Model/AlbumCromoDAO.java
+++ exercici_cromos_2025/menu_collection/src/Model/AlbumCromoDAO.java
@@ -16,5 +16,40 @@ public class AlbumCromoDAO {
         this.NUM_MAX_COLECCION = num;
         
     }
+    
+    /**
+     * Afegeix el cromo a la nostra col·lecció 
+     * @param afegir Cromo que vull afegir al  album
+     * @return true si s'afegeix /false si es fica a repes
+     * @throws NumberOutofAlbum quant el cromo a afegir no es de la col·lecció
+     */
+    public boolean afegirCromo(Cromo afegir) throws NumberOutofAlbum
+            {
+    	
+                    if (afegir.getNum()> NUM_MAX_COLECCION)
+                    {
+                        throw new NumberOutofAlbum(NUM_MAX_COLECCION);
+                    }
+                    else
+                    {
+                        if (album.contains(afegir)) 
+                        {
+                            caixa_repes.add(afegir);
+                            return false;
+                        }
+                        else
+                        {
+                            album.add(afegir);
+                            return true;
+                        }  
+                    }
+            }
+    
+    public boolean afegirCromo(int num_cromo, String nom_cromo, TipoBrawler tipus) throws NumberOutofAlbum
+    {
+        Cromo afegir = new Cromo(num_cromo, nom_cromo, tipus);
+        return this.afegirCromo(afegir);
+        
+    }
 	
 }
