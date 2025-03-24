4cfa1fb161cb18dd9d9d75a21ac320b3c9f11f37
pauca881 <lopezpaud@gmail.com>
2025-03-24 15:18:32 +0100

    Module info deleted

---
+++ test_JDBC/src/test_JDBC/main.java
@@ -0,0 +1,35 @@
+package test_JDBC;
+
+public class main {
+	
+	public static void main(String[] args) {
+		
+        /*
+         * 
+         * INSTALAR XAMPP I CONECTOR JDBC 
+         
+         CREATE DATABASE IF NOT EXISTS nom_usuari;
+		 CREATE USER 'nom_usuari'@'localhost' IDENTIFIED BY 'nom_usuari';
+		 GRANT ALL PRIVILEGES ON nom_usuari.* TO 'nom_usuari'@'localhost';
+		 FLUSH PRIVILEGES;
+
+         
+         
+        -- Creació de la taula 'countries'
+        CREATE TABLE countries (
+            id INT AUTO_INCREMENT PRIMARY KEY,  
+            nom VARCHAR(100) NOT NULL,
+            capital VARCHAR(100) NOT NULL
+        );
+        
+        INSERT INTO countries (nom, capital) VALUES
+            ('Espanya', 'Madrid'),
+            ('França', 'París'),
+            ('Itàlia', 'Roma'),
+            ('Alemanya', 'Berlín'),
+            ('Portugal', 'Lisboa');
+        */
+	}
+	
+
+}
