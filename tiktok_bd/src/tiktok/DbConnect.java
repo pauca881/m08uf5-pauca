2ff821ac16e40122ec33750fa5a959ad8a8883af
pauca881 <lopezpaud@gmail.com>
2025-03-28 22:28:36 +0100

    JAR Driver and connection

--- tiktok_bd/src/tiktok/DbConnect.java
+++ tiktok_bd/src/tiktok/DbConnect.java
@@ -4,15 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
     
+//Com afegir el JAR al projecte Eclipse?
+//Click Dret al projecte -> Build Path -> Add External Libraries -> Seleccionem el driver .jar
 
 public final class DbConnect {
     
     static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     static final String PROTOCOL = "jdbc:mysql:";
     static final String HOST = "127.0.0.1";
-    static final String BD_NAME = "dbcountry";
-    static final String USER = "usrcountry";
-    static final String PASSWORD = "pswcountry";
+    static final String BD_NAME = "tiktok";
+    static final String USER = "root";
+    static final String PASSWORD = "";
     static final String PARAMS = "useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     
     public static void loadDriver() throws ClassNotFoundException {
