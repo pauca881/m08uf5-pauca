4b9aa2905881055cd625de5a32240232c29f8852
Pauk <56691890+pauca881@users.noreply.github.com>
2025-04-06 20:32:15 +0200

    commit saver

--- PokemonGO/src/app/app.java
+++ PokemonGO/src/app/app.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
+//commit saver
 import model.Entrenador;
 import model.EntrenadorDAO;
 import model.Pokemon;
