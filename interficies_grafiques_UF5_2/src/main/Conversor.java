05bafc80cec4e72e01a33a1087dcf2b20b890c7a
pauca881 <lopezpaud@gmail.com>
2025-04-27 18:36:41 +0200

    interficies

---
+++ interficies_grafiques_UF5_2/src/main/Conversor.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class Conversor extends JFrame {
+
+	
+	
+}
