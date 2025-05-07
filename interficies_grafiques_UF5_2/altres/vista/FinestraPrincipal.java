68cc80eec4fee281156f690ddb6ec788309d86ca
Pauk <56691890+pauca881@users.noreply.github.com>
2025-05-07 21:14:18 +0200

    packages and class

--- interficies_grafiques_UF5_2/altres/vista/FinestraPrincipal.java
+++ interficies_grafiques_UF5_2/altres/vista/FinestraPrincipal.java
@@ -1,1 +1,35 @@
+package vista;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+
+public class FinestraPrincipal extends JFrame implements ActionListener{
+
+    JTextField textoentrada;
+    PanelRadioButtons opciones_conversion;
+
+    ActionListener listener;
+    ActionListener lis1;
+    
+
+  
+}
