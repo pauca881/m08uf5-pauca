5df8ba6f36ff81f6979503f386fa80d19fbafa1e
Pauk <56691890+pauca881@users.noreply.github.com>
2025-05-07 20:00:41 +0200

    first part PanelRadioButton

--- interficies_grafiques_UF5_2/altres/vista/PanelRadioButtons.java
+++ interficies_grafiques_UF5_2/altres/vista/PanelRadioButtons.java
@@ -1,1 +1,24 @@
+package vista;
 
+import java.awt.GridLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class PanelRadioButtons extends JPanel{
+
+    JRadioButton euro,dolar,peso,Yen,Dirham,Rublo;
+
+  public PanelRadioButtons(){
+      //2 filas 3 columnas
+      this.setLayout(new GridLayout(2, 3));
+      initComponents();
+    
+  }
+
+
+
+
+
+  
+}//end
