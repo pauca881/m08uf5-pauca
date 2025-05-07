5cd1a3df8ea1e9318172dfb0e93bd3e0866469fd
Pauk <56691890+pauca881@users.noreply.github.com>
2025-05-07 20:07:35 +0200

    Getters Botones

--- interficies_grafiques_UF5_2/altres/vista/PanelRadioButtons.java
+++ interficies_grafiques_UF5_2/altres/vista/PanelRadioButtons.java
@@ -16,7 +16,65 @@ public class PanelRadioButtons extends JPanel{
     
   }
 
+    private void initComponents(){
+    
+    ButtonGroup grupo = new ButtonGroup();
+        
+        euro = new JRadioButton("Euros");
+        dolar = new JRadioButton("Dolares");
+        peso = new JRadioButton("Pesos");
+        Yen = new JRadioButton("Yen");
+        Dirham = new JRadioButton("Dirham");
+        Rublo = new JRadioButton("Rublos");
 
+        //Se agregan al JPanel PanelRadioButtons ( extends JPanel )
+        this.add(euro);
+        this.add(dolar);
+        this.add(peso);
+        this.add(Yen);
+        this.add(Dirham);
+        this.add(Rublo);
+
+        //Se agregan al grupo de botones
+        grupo.add(euro);
+        grupo.add(dolar);
+        grupo.add(peso);
+        grupo.add(Yen);
+        grupo.add(Dirham);
+        grupo.add(Rublo);
+        
+    
+    }
+
+    //Encapsulamiento
+    //Permite acceder a los botones. Por ejemplo:
+    //JRadioButton botonEuro = panel.getEuro();
+    //if (botonEuro.isSelected
+    
+    public JRadioButton getEuro() {
+        return euro;
+    }
+
+    public JRadioButton getDolar() {
+        return dolar;
+    }
+
+    public JRadioButton getPeso() {
+        return peso;
+    }
+
+    public JRadioButton getYen() {
+        return Yen;
+    }
+
+    public JRadioButton getDirham() {
+        return Dirham;
+    }
+
+    public JRadioButton getRublo() {
+        return Rublo;
+    }
+    
 
 
 
