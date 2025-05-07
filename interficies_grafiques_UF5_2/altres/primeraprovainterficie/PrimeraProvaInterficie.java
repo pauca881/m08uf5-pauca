12818fcbd8d44ed677790b04fdb99486adfe99f8
Pauk <56691890+pauca881@users.noreply.github.com>
2025-05-07 19:55:23 +0200

    Nova primera finestra

--- interficies_grafiques_UF5_2/altres/primeraprovainterficie/PrimeraProvaInterficie.java
+++ interficies_grafiques_UF5_2/altres/primeraprovainterficie/PrimeraProvaInterficie.java
@@ -1,34 +1,13 @@
-package vista;
+package primeraprovainterficie;
 
-import java.awt.GridLayout;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
+import javax.swing.JFrame;
+import vista.FinestraPrincipal;
 
+public class PrimeraProvaInterficie {
 
-public class PanelRadioButtons extends JPanel {
+    public static void main(String[] args){
 
-    public PanelRadioButtons() {
-        this.setLayout(new GridLayout(2, 3));
-        initComponents();
+        FinestraPrincipal finestra = new FinestraPrincipal("Finestra 1");
+    
     }
-
-    private void initComponents() {
-        JRadioButton euro = new JRadioButton("Euros");
-        JRadioButton dolar = new JRadioButton("Dolares");
-        JRadioButton peso = new JRadioButton("Pesos");
-        JRadioButton Yen = new JRadioButton("Yen");
-        JRadioButton Dirham = new JRadioButton("Dirham");
-        JRadioButton Rublo = new JRadioButton("Rublos");
-        
-        this.add(euro);
-        this.add(dolar);
-        this.add(peso);
-        this.add(Yen);
-        this.add(Dirham);
-        this.add(Rublo);
-        
-    }
-    
-    
-    
 }
