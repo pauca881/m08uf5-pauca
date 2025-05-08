fe058a56d45751dc6e4701917c0f2f55434a4c4e
pauca881 <lopezpaud@gmail.com>
2025-05-08 22:01:08 +0200

    capturar pokemon project

---
+++ capturar_pokemon_UF5_2/src/capturar/capturar.java
@@ -0,0 +1,15 @@
+package capturar;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class capturar extends JFrame {
+
+	
+	//constructorJFrame
+	public capturar() {}
+	
+	
+}
