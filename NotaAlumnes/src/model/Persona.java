c24f83c3133b906ba6a82c1839e85f72ffe4f69c
pauca881 <lopezpaud@gmail.com>
2025-05-09 20:31:40 +0200

    objectes i swing invoke later

--- NotaAlumnes/src/model/Persona.java
+++ NotaAlumnes/src/model/Persona.java
@@ -1,6 +1,29 @@
 package model;
 
 public class Persona {
+	
+	
+    private String nom;
+    private String dni;
+    private double nota;
+    
+    public Persona(String nom, String dni, double nota) {
+    	
+    	this.nom = nom;
+    	this.dni = dni;
+        this.nota = nota;
+
+    	
+    }
+    
+    public String getNom() { return nom; }
+    public String getDni() { return dni; }
+    public double getNota() { return nota; }
+
+    public void setNota(double nota) { this.nota = nota; }
+    
+    
+    
 
 	
 }
--- NotaAlumnes/src/notaalumnes/NotaAlumnes.java
+++ NotaAlumnes/src/notaalumnes/NotaAlumnes.java
@@ -10,6 +10,12 @@ public class NotaAlumnes {
 		
 		GestorAlumnesDAO model = new GestorAlumnesDAO();
 		
+		//Assegura que el codi que interactua amb la interfaç gràfica
+		//s'executi en el fil correcte: Event Dispatch Thread
+		
+		//Sense anar a fons, fa que tot el codi s'executi en un únic fil,
+		//no està dissenyat perque diferents fils modifiquin la interfaç gràfica
+		
 		SwingUtilities.invokeLater(() -> {
 			
 			PantallaAlumnes finestra = new PantallaAlumnes(model);
