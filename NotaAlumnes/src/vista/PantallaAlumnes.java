e637dc06fd09039266ed6bfa9f9bf48a65270361
pauca881 <lopezpaud@gmail.com>
2025-05-09 20:02:17 +0200

    panell alumnes

--- NotaAlumnes/src/vista/PantallaAlumnes.java
+++ NotaAlumnes/src/vista/PantallaAlumnes.java
@@ -2,6 +2,23 @@ package vista;
 
 import javax.swing.JFrame;
 
+import model.GestorAlumnesDAO;
+
 public class PantallaAlumnes extends JFrame{
+	
+	
+	public PantallaAlumnes(GestorAlumnesDAO model) {
+		
+		
+		carregarPanell(model);
+		
+		
+	}
+
+	private void carregarPanell(GestorAlumnesDAO model) {
+
+			this.setContentPane(new PanellAlumnes(model));
+		
+	}
 
 }
