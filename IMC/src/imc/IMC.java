b17069d76e207a58148840a9e24febb31e029e2c
pauca881 <lopezpaud@gmail.com>
2025-05-11 15:21:53 +0200

    calcul

--- IMC/src/imc/IMC.java
+++ IMC/src/imc/IMC.java
@@ -1,5 +1,12 @@
 package imc;
 
 public class IMC {
+	
+	public double IMCCalc(double height, double weight) {
+		
+		double IMC = weight / (height*height);
+		return IMC;
+		
+	}
 
 }
--- IMC/src/model/calculIMC.java
+++ IMC/src/model/calculIMC.java
@@ -1,7 +1,22 @@
 package model;
 
+import javax.swing.SwingUtilities;
+import vista.VentanaPrincipal;
+
 public class calculIMC {
 	
-	
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override	
+			public void run() {
+					VentanaPrincipal window = new VentanaPrincipal();
+				}
+			
+			
+			
+		});
+		
+	}
 
 }
