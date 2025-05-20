7370adc6f1a4031c19fba4b1c82faa2f3de334e6
Pauk <56691890+pauca881@users.noreply.github.com>
2025-05-20 19:57:29 +0200

    entrades

--- examenUF5_interficies/solucio/EntradesConsola.java
+++ examenUF5_interficies/solucio/EntradesConsola.java
@@ -1,1 +1,33 @@
+package entradesconsola;
 
+import dades.LlistaAssistents;
+import java.io.IOException;
+import model.Assistent;
+import repositori.RepositoriAssistents;
+import vista.VistaFestival;
+import java.util.List;
+/**
+ *
+ * @author mabardaji
+ */
+public class EntradesConsola {
+
+ public static void main(String[] args) {
+   
+         RepositoriAssistents repo = new RepositoriAssistents();
+         LlistaAssistents totes_entrades = new LlistaAssistents();
+         VistaFestival vista= new VistaFestival();
+         String fitxer = vista.demanarNomFitxer();
+       try {    
+         List<Assistent> inicials = repo.carregarAssistents(fitxer);
+         totes_entrades.carregaLlistaAssistents(inicials);
+         
+         
+         vista = new VistaFestival(totes_entrades, repo);
+         vista.mostrarMenu();
+     } catch (IOException ex) {
+         vista.mostrarMensaje(ex.getMessage());
+     }
+    }
+    
+}
