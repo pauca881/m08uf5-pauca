54989cd3cfb20ec9bcd6175c508be291e5d8edfa
Pauk <56691890+pauca881@users.noreply.github.com>
2025-05-21 22:20:27 +0200

    assistents

--- examenUF5_interficies/solucio/dades/LlistaAssistents.java
+++ examenUF5_interficies/solucio/dades/LlistaAssistents.java
@@ -1,1 +1,14 @@
+package dades;
 
+import model.Assistent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LlistaAssistents {
+    private List<Assistent> assistents;
+
+    public LlistaAssistents() {
+        this.assistents = new ArrayList<>();
+    }
