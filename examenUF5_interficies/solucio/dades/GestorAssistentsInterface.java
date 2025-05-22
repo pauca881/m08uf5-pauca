4ff202b86f151dd2d2e1318182c0ed4b67085601
Pauk <56691890+pauca881@users.noreply.github.com>
2025-05-22 21:10:59 +0200

    metodes principals

--- examenUF5_interficies/solucio/dades/GestorAssistentsInterface.java
+++ examenUF5_interficies/solucio/dades/GestorAssistentsInterface.java
@@ -6,5 +6,14 @@ import model.Assistent;
 
 public interface GestorAssistentsInterface {
 
+  
+    /* Afegeix un nou assistent a la llista interna */
+    void afegirAssistent(Assistent a);
+
+    /* Retorna la llista completa d�assistents */
+    List<Assistent> getTots();
+
+    /* Calcula i retorna l�edat mitjana dels assistents */
+    double calcularEdatMitjana();
 
 }
